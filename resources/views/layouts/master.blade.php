182827589bafd8743654bdda245b144a20bc0e2a
Lyon <mulyonoeko.prastyo@gmail.com>
2018-11-12 06:37:54 +0700

    Modifikasi Menu AdminLTE (Bagian Awal)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -160,11 +160,29 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           {{-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library --}}
-          <li class="nav-item has-treeview menu-open">
-            <a href="#" class="nav-link active">
+          <li class="nav-item">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
+                {{-- <span class="right badge badge-danger">New</span> --}}
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                User
+                {{-- <span class="right badge badge-danger">New</span> --}}
+              </p>
+            </a>
+          </li>
+          <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-newspaper"></i>
+              <p>
+                Artikel
                 <i class="right fa fa-angle-left"></i>
               </p>
             </a>
@@ -185,10 +203,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
+              <i class=" nav-icon fas fa-power-off"></i>
               <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
+                Keluar
               </p>
             </a>
           </li>
