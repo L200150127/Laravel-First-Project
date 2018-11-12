fc847c2d43b6b4503f39a6d29c6a666c8bfcd4d3
Lyon <mulyonoeko.prastyo@gmail.com>
2018-11-13 06:33:10 +0700

    Modifikasi Menu AdminLTE (Bagian Ketiga), Menambah variabel warna dan memberi warna ikon pada menu sidebar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,9 +1,7 @@
-<!DOCTYPE html>
-{{--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
---}}
+<!DOCTYPE html> {{-- This is a starter template page. Use this page to start your new project from scratch. This page gets rid of all links
+and provides the needed markup only. --}}
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,40 +14,41 @@ scratch. This page gets rid of all links and provides the needed markup only.
   {{-- Compiled Stylesheet --}}
   <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 </head>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper" id="app">
+  <div class="wrapper" id="app">
 
-  {{-- Navbar --}}
-  <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
-    {{-- Left navbar links --}}
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
-      </li>
-    </ul>
+    {{-- Navbar --}}
+    <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+      {{-- Left navbar links --}}
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+        </li>
+      </ul>
 
-    {{-- SEARCH FORM --}}
-    <form class="form-inline ml-3">
-      <div class="input-group input-group-sm">
-        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-          <button class="btn btn-navbar" type="submit">
+      {{-- SEARCH FORM --}}
+      <form class="form-inline ml-3">
+        <div class="input-group input-group-sm">
+          <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-navbar" type="submit">
             <i class="fa fa-search"></i>
           </button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
 
-    {{-- Right navbar links --}}
-    <ul class="navbar-nav ml-auto">
-      {{-- Messages Dropdown Menu --}}
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
+      {{-- Right navbar links --}}
+      <ul class="navbar-nav ml-auto">
+        {{-- Messages Dropdown Menu --}}
+        <li class="nav-item dropdown">
+          <a class="nav-link" data-toggle="dropdown" href="#">
           <i class="fa fa-comments-o"></i>
           <span class="badge badge-danger navbar-badge">3</span>
         </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <a href="#" class="dropdown-item">
+          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+            <a href="#" class="dropdown-item">
             {{-- Message Start --}}
             <div class="media">
               <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
@@ -64,8 +63,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </div>
             {{-- Message End --}}
           </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
             {{-- Message Start --}}
             <div class="media">
               <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
@@ -80,8 +79,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </div>
             {{-- Message End --}}
           </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
             {{-- Message Start --}}
             <div class="media">
               <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
@@ -96,217 +95,205 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </div>
             {{-- Message End --}}
           </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-        </div>
-      </li>
-      {{-- Notifications Dropdown Menu --}}
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+          </div>
+        </li>
+        {{-- Notifications Dropdown Menu --}}
+        <li class="nav-item dropdown">
+          <a class="nav-link" data-toggle="dropdown" href="#">
           <i class="fa fa-bell-o"></i>
           <span class="badge badge-warning navbar-badge">15</span>
         </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-header">15 Notifications</span>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
+          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+            <span class="dropdown-header">15 Notifications</span>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
             <i class="fa fa-envelope mr-2"></i> 4 new messages
             <span class="float-right text-muted text-sm">3 mins</span>
           </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
             <i class="fa fa-users mr-2"></i> 8 friend requests
             <span class="float-right text-muted text-sm">12 hours</span>
           </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
             <i class="fa fa-file mr-2"></i> 3 new reports
             <span class="float-right text-muted text-sm">2 days</span>
           </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-        </div>
-      </li>
-    </ul>
-  </nav>
-  {{-- /.navbar --}}
-
-  {{-- Main Sidebar Container --}}
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    {{-- Brand Logo --}}
-    <a href="/home" class="brand-link">
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+          </div>
+        </li>
+      </ul>
+    </nav>
+    {{-- /.navbar --}} {{-- Main Sidebar Container --}}
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      {{-- Brand Logo --}}
+      <a href="/home" class="brand-link">
     <img src="{{ asset('img/admin.svg') }}" alt="Logo Situs" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">StartUp.</span><span class="brand-text font-weight-light text-primary">Inc</span>
-    </a>
-
-    {{-- Menambahkan credit --}}
-    {{-- <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="User">User</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"     title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> --}}
-    
-    {{-- Sidebar --}}
-    <div class="sidebar">
-      {{-- Sidebar user panel (optional) --}}
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{ asset('img/user_admin.svg') }}" class="img-circle elevation-2" alt="Gambar User Admin">
+    </a> {{-- Menambahkan credit --}} {{--
+      <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="User">User</a> from <a href="https://www.flaticon.com/"
+          title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0"
+          target="_blank">CC 3.0 BY</a></div> --}} {{-- Sidebar --}}
+      <div class="sidebar">
+        {{-- Sidebar user panel (optional) --}}
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="image">
+            <img src="{{ asset('img/user_admin.svg') }}" class="img-circle elevation-2" alt="Gambar User Admin">
+          </div>
+          <div class="info">
+            <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+          </div>
         </div>
-        <div class="info">
-        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
-        </div>
-      </div>
 
-      {{-- Sidebar Menu --}}
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          {{-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library --}}
-          <li class="nav-item">
-            <router-link to="/dashboard" class="nav-link">
-              <i class="nav-icon fas fa-home"></i>
-              <p>
-                Dashboard
-                {{-- <span class="right badge badge-danger">New</span> --}}
-              </p>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-users"></i>
+        {{-- Sidebar Menu --}}
+        <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            {{-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library --}}
+            <li class="nav-item">
+              <router-link to="/dashboard" class="nav-link">
+                <i class="nav-icon fas fa-home text-primary"></i>
+                <p>
+                  Dashboard {{-- <span class="right badge badge-danger">New</span> --}}
+                </p>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-users text-warning"></i>
               <p>
                 User
                 {{-- <span class="right badge badge-danger">New</span> --}}
               </p>
             </a>
-          </li>
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-newspaper"></i>
+            </li>
+            <li class="nav-item has-treeview">
+              <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-newspaper text-white"></i>
               <p>
                 Artikel
                 <i class="right fa fa-angle-left"></i>
               </p>
             </a>
-            <ul class="nav nav-treeview">
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <router-link to="#" class="nav-link">
+                    <i class="fas fa-pencil-alt nav-icon"></i>
+                    <p>Buat Artikel</p>
+                  </router-link>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <router-link to="#" class="nav-link">
+                <i class=" nav-icon fas fa-images text-info"></i>
+                <p>
+                  Galeri Foto
+                </p>
+              </router-link>
+            </li>
+            <li class="nav-item">
               <li class="nav-item">
                 <router-link to="#" class="nav-link">
-                  <i class="fas fa-pencil-alt nav-icon"></i>
-                  <p>Buat Artikel</p>
+                  <i class=" nav-icon fas fa-clipboard-list text-success"></i>
+                  <p>
+                    Agenda
+                  </p>
                 </router-link>
               </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <router-link to="#" class="nav-link">
-              <i class=" nav-icon fas fa-images"></i>
-              <p>
-                Galeri Foto
-              </p>
-            </router-link>
-          </li>
-          <li class="nav-item">
-          <li class="nav-item">
-            <router-link to="#" class="nav-link">
-              <i class=" nav-icon fas fa-clipboard-list"></i>
-              <p>
-                Agenda
-              </p>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="#" class="nav-link">
-              <i class=" nav-icon fas fa-chalkboard-teacher"></i>
-              <p>
-                Data Guru
-              </p>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="#" class="nav-link">
-              <i class=" nav-icon fas fa-book-reader"></i>
-              <p>
-                Data Siswa
-              </p>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="#" class="nav-link">
-              <i class=" nav-icon fas fa-archive"></i>
-              <p>
-                File Manajer
-              </p>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/profile" class="nav-link">
-              <i class=" nav-icon fas fa-user"></i>
-              <p>
-                Profil
-              </p>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class=" nav-icon fas fa-power-off"></i>
-              <p>
-                Keluar
-              </p>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      {{-- /.sidebar-menu --}}
-    </div>
-    {{-- /.sidebar --}}
-  </aside>
+              <li class="nav-item">
+                <router-link to="#" class="nav-link">
+                  <i class=" nav-icon fas fa-chalkboard-teacher text-teal"></i>
+                  <p>
+                    Data Guru
+                  </p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="#" class="nav-link">
+                  <i class=" nav-icon fas fa-book-reader text-orange"></i>
+                  <p>
+                    Data Siswa
+                  </p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="#" class="nav-link">
+                  <i class=" nav-icon fas fa-archive text-cyan"></i>
+                  <p>
+                    File Manajer
+                  </p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/profile" class="nav-link">
+                  <i class=" nav-icon fas fa-user text-yellow"></i>
+                  <p>
+                    Profil
+                  </p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('logout') }}" 
+                onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                  <i class=" nav-icon fas fa-power-off text-danger"></i>
+                  <p>{{ __('Logout') }}</p>
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+                </form>
+              </li>
+          </ul>
+        </nav>
+        {{-- /.sidebar-menu --}}
+      </div>
+      {{-- /.sidebar --}}
+    </aside>
 
-  {{-- Content Wrapper. Contains page content --}}
-  <div class="content-wrapper">
-    {{-- Content Header (Page header) --}}
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Starter Page</h1>
-          </div>{{-- /.col --}}
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item">Dashboard</li>
-            </ol>
-          </div>{{-- /.col --}}
-        </div>{{-- /.row --}}
-      </div>{{-- /.container-fluid --}}
+    {{-- Content Wrapper. Contains page content --}}
+    <div class="content-wrapper">
+      {{-- Content Header (Page header) --}}
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0 text-dark">Starter Page</h1>
+            </div>{{-- /.col --}}
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item">Dashboard</li>
+              </ol>
+            </div>{{-- /.col --}}
+          </div>{{-- /.row --}}
+        </div>{{-- /.container-fluid --}}
+      </div>
+      {{-- /.content-header --}} {{-- Main content --}}
+      <div class="content">
+        <div class="container-fluid">
+          {{-- route outlet --}} {{-- component matched by the route will render here --}}
+          <router-view></router-view>
+        </div>{{-- /.container-fluid --}}
+      </div>
+      {{-- /.content --}}
     </div>
-    {{-- /.content-header --}}
-
-    {{-- Main content --}}
-    <div class="content">
-      <div class="container-fluid">
-        {{-- route outlet --}}
-        {{-- component matched by the route will render here --}}
-        <router-view></router-view>
-      </div>{{-- /.container-fluid --}}
-    </div>
-    {{-- /.content --}}
+    {{-- /.content-wrapper --}} {{-- Main Footer --}}
+    <footer class="main-footer">
+      {{-- To the right --}}
+      <div class="float-right d-none d-sm-inline">
+        Anything you want
+      </div>
+      {{-- Default to the left --}}
+      <strong>Copyright &copy; 2014-2018 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    </footer>
   </div>
-  {{-- /.content-wrapper --}}
-
-
-  {{-- Main Footer --}}
-  <footer class="main-footer">
-    {{-- To the right --}}
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
-    {{-- Default to the left --}}
-    <strong>Copyright &copy; 2014-2018 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
-</div>
-{{-- ./wrapper --}}
-
-{{-- REQUIRED SCRIPTS --}}
-
-{{-- Compiled Script --}}
-<script src="{{ asset('js/app.js') }}"></script> 
+  {{-- ./wrapper --}} {{-- REQUIRED SCRIPTS --}} {{-- Compiled Script --}}
+  <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
